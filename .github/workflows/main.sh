d9b03a06c57b9f8b637d8dad27565ac3f6ca88ce
ZhuYitou <71212752+ZhuYitou@users.noreply.github.com>
2021-06-29 18:38:33 +0800

    Update main.sh

--- .github/workflows/main.sh
+++ .github/workflows/main.sh
@@ -1,4 +1,4 @@
-cat>>1.txt<<EOF
+cat 1.txt<<EOF
 \n
 EOF
-2.txt # g++ zyx_1184.cpp -o test && ./test < in.txt > out.txt && cat out.txt
+#2.txt # g++ zyx_1184.cpp -o test && ./test < in.txt > out.txt && cat out.txt
